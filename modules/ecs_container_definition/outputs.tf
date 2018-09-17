5b8ae6f9698f3e97fde872c0e1de7548461a8856
maartenvanderhoef <maarten@blinkist.com>
2018-09-17 09:09:48 +0200

    + ecs_container_definition, unsure what to do with lic

---
+++ modules/ecs_container_definition/outputs.tf
@@ -0,0 +1,12 @@
+output "json" {
+  description = "JSON encoded container definitions for use with other terraform resources such as aws_ecs_task_definition."
+
+  # The following hack is required to overcome TF automatic type conversions which lead to issues with the resulting json types.
+  # Conversion happens by using the built-in `replace` function in this order:
+  #  - Convert `""`, `{}`, `[]`, and `[""]` to `null`
+  #  - Convert `"true"` and `"false"` to `true` and `false`
+  #  - Convert quoted numbers (e.g. `"123"`) to `123`.
+  #  - Convert "string:true" to "true"
+  # Environment variables are kept as strings.
+  value = "${replace(replace(replace(jsonencode(local.container_definitions), "/(\\[\\]|\\[\"\"\\]|\"\"|{})/", "null"), "/(\"[^v][[:alpha:]]+\":)\"([0-9]+\\.?[0-9]*|true|false)\"/", "$1$2"), "string:", "")}"
+}
