314748e97e9afe2fb092940995c680742309dce7
maartenvanderhoef <maarten@blinkist.com>
2018-11-07 20:42:17 +0100

    cognito listener rule needs to be outputted

--- modules/alb_handling/main.tf
+++ modules/alb_handling/main.tf
@@ -264,5 +264,5 @@ resource "aws_lb_listener_rule" "host_based_routing_ssl_custom_listen_host_cogni
 
 # This is an output the ecs_service depends on. This to make sure the target_group is attached to an alb before adding to a service. The actual content is useless
 output "aws_lb_listener_rules" {
-  value = ["${concat(aws_lb_listener_rule.host_based_routing.*.arn,aws_lb_listener_rule.host_based_routing_custom_listen_host.*.arn, list())}"]
+  value = ["${concat(aws_lb_listener_rule.host_based_routing.*.arn,aws_lb_listener_rule.host_based_routing_custom_listen_host.*.arn, aws_lb_listener_rule.host_based_routing.host_based_routing_ssl_custom_listen_host_cognito_auth.*.arn, list())}"]
 }
