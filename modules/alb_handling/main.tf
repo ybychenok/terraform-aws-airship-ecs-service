68b6ffd423c2ee20cae7f55b114cafe264aca4fb
maartenvanderhoef <maarten@blinkist.com>
2018-11-07 20:27:59 +0100

    Extra doc

--- modules/alb_handling/main.tf
+++ modules/alb_handling/main.tf
@@ -61,7 +61,7 @@ resource "aws_lb_target_group" "service" {
 ##
 ## An aws_lb_listener_rule will only be created when a service has a load balancer attached
 resource "aws_lb_listener_rule" "host_based_routing" {
-  count = "${var.create && local.route53_record_type != "NONE" ? 1 : 0 }"
+  count = "${var.create && ! var.redirect_http_to_https && local.route53_record_type != "NONE" ? 1 : 0 }"
 
   listener_arn = "${var.lb_listener_arn}"
 
@@ -82,9 +82,37 @@ resource "aws_lb_listener_rule" "host_based_routing" {
 }
 
 ##
+## aws_lb_listener_rule which redirects http to https
+resource "aws_lb_listener_rule" "host_based_routing_redirect_to_https" {
+  count = "${var.create && var.redirect_http_to_https && local.route53_record_type != "NONE" ? 1 : 0 }"
+
+  listener_arn = "${var.lb_listener_arn}"
+
+  action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  condition {
+    field = "host-header"
+
+    values = ["${local.route53_record_type == "CNAME" ?
+       join("",aws_route53_record.record.*.fqdn)
+       :
+       join("",aws_route53_record.record_alias_a.*.fqdn)
+       }"]
+  }
+}
+
+##
 ## An aws_lb_listener_rule will only be created when a service has a load balancer attached
 resource "aws_lb_listener_rule" "host_based_routing_ssl" {
-  count = "${var.create && local.route53_record_type != "NONE" ? 1 : 0 }"
+  count = "${var.create && ! var.cognito_auth_enabled && local.route53_record_type != "NONE" ? 1 : 0 }"
 
   listener_arn = "${var.lb_listener_arn_https}"
 
@@ -96,7 +124,40 @@ resource "aws_lb_listener_rule" "host_based_routing_ssl" {
   condition {
     field = "host-header"
 
-    values = ["${local.route53_record_type == "CNAME" ? 
+    values = ["${local.route53_record_type == "CNAME" ?
+       join("",aws_route53_record.record.*.fqdn)
+       :
+       join("",aws_route53_record.record_alias_a.*.fqdn)
+       }"]
+  }
+}
+
+##
+## An aws_lb_listener_rule will only be created when a service has a load balancer attached
+resource "aws_lb_listener_rule" "host_based_routing_ssl_cognito_auth" {
+  count = "${var.create && var.cognito_auth_enabled && local.route53_record_type != "NONE" ? 1 : 0 }"
+
+  listener_arn = "${var.lb_listener_arn_https}"
+
+  action {
+    type = "authenticate-cognito"
+
+    authenticate_cognito {
+      user_pool_arn       = "${var.cognito_user_pool_arn}"
+      user_pool_client_id = "${var.cognito_user_pool_client_id}"
+      user_pool_domain    = "${var.cognito_user_pool_domain}"
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.service.arn}"
+  }
+
+  condition {
+    field = "host-header"
+
+    values = ["${local.route53_record_type == "CNAME" ?
        join("",aws_route53_record.record.*.fqdn)
        :
        join("",aws_route53_record.record_alias_a.*.fqdn)
@@ -117,7 +178,7 @@ data "template_file" "custom_listen_host" {
 ##
 ## An aws_lb_listener_rule will only be created when a service has a load balancer attached
 resource "aws_lb_listener_rule" "host_based_routing_custom_listen_host" {
-  count = "${var.create ? length(var.custom_listen_hosts) : 0 }"
+  count = "${var.create && ! var.redirect_http_to_https ? length(var.custom_listen_hosts) : 0 }"
 
   listener_arn = "${var.lb_listener_arn}"
 
@@ -133,11 +194,62 @@ resource "aws_lb_listener_rule" "host_based_routing_custom_listen_host" {
 }
 
 ##
+## aws_lb_listener_rule which redirects http to https for the customer listen hosts
+resource "aws_lb_listener_rule" "host_based_routing_custom_listen_host_redirect_to_https" {
+  count = "${var.create && var.redirect_http_to_https ? length(var.custom_listen_hosts) : 0 }"
+
+  listener_arn = "${var.lb_listener_arn}"
+
+  action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${data.template_file.custom_listen_host.*.rendered[count.index]}"]
+  }
+}
+
+##
 ## An aws_lb_listener_rule will only be created when a service has a load balancer attached
 resource "aws_lb_listener_rule" "host_based_routing_ssl_custom_listen_host" {
-  count = "${var.create ? length(var.custom_listen_hosts) : 0 }"
+  count = "${var.create && ! var.cognito_auth_enabled ? length(var.custom_listen_hosts) : 0 }"
 
   listener_arn = "${var.lb_listener_arn_https}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.service.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${data.template_file.custom_listen_host.*.rendered[count.index]}"]
+  }
+}
+
+##
+## An aws_lb_listener_rule will only be created when a service has a load balancer attached
+resource "aws_lb_listener_rule" "host_based_routing_ssl_custom_listen_host_cognito_auth" {
+  count = "${var.create && var.cognito_auth_enabled ? length(var.custom_listen_hosts) : 0 }"
+
+  listener_arn = "${var.lb_listener_arn_https}"
+
+  action {
+    type = "authenticate-cognito"
+
+    authenticate_cognito {
+      user_pool_arn       = "${var.cognito_user_pool_arn}"
+      user_pool_client_id = "${var.cognito_user_pool_client_id}"
+      user_pool_domain    = "${var.cognito_user_pool_domain}"
+    }
+  }
 
   action {
     type             = "forward"
