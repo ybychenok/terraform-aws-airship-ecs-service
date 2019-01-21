1f77d89219a5748e57abf32d8890e0dfbb890d03
Łukasz German <10104699+LukGerman@users.noreply.github.com>
2019-01-21 21:33:32 +0100

    Add more output variables (#42)
    * More outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "ecs_taskrole_name" {
 output "lb_target_group_arn" {
   value = "${module.alb_handling.lb_target_group_arn}"
 }
+
+output "task_execution_role_arn" {
+  value = "${module.iam.ecs_task_execution_role_arn}"
+}
+
+output "aws_ecs_task_definition_arn" {
+  value = "${module.ecs_task_definition_selector.selected_task_definition_for_deployment}"
+}
+
+output "aws_ecs_task_definition_family" {
+  value = "${module.ecs_task_definition.aws_ecs_task_definition_family}"
+}
